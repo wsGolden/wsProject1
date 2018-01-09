aaa888ae54b59c053e375baa318621ea6802a9c5
wsGolden <573012197@qq.com>
2018-01-09 15:57:40 +0800

    head

---
+++ css/gulpfile.js
@@ -0,0 +1,21 @@
+//引入所需插件
+var gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	rename = require("gulp-rename"),
+	cssnano = require("gulp-cssnano"),
+	imagemin = require("gulp-imagemin"),
+	concat=require("gulp-concat"),
+	jshint=require("gulp-jshint"),
+	uglify=require("gulp-uglify")
+	
+//编译sass
+gulp.task("sass",function(){
+	return gulp.src("../sass/*.scss").pipe(sass({style : "expanded"})).pipe(gulp.dest("css")).pipe(rename({"suffix" : ".min"})).pipe(cssnano()).pipe(gulp.dest("css"));
+})
+//监听任务
+gulp.task("watch",function(){
+	gulp.watch("../sass/*.scss",["sass"]);
+})
+gulp.task("script",function(){
+	return gulp.src("js/*.js").pipe(rename({suffix : ".min"})).pipe(uglify()).pipe(gulp.dest("js"));
+})
